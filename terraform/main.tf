100bf6c524e018c2e1b5deae192c64a17d671e43
The Punisher <maxnuwan@gmail.com>
2025-10-08 14:16:19 +0800

    Create Shared Services Account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,25 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "lz-shared@vmgeeks.com"
+    AccountName               = "LZ-Shared"
+    ManagedOrganizationalUnit = "Infrastructure"
+    SSOUserEmail              = "lz-shared@vmgeeks.com"
+    SSOUserFirstName          = "LZ"
+    SSOUserLastName           = "Shared"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Environment" = "Shared"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "VMGeeks"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
   }
 
   custom_fields = {
-    group = "non-prod"
+    group = "prod"
   }
 
   account_customizations_name = "sandbox"
